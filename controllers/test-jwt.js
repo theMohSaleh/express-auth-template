29196f2cbaaeb2449d80f7d4e70975ef0da6f4d8
Mohamed Saleh <mohxbox36021@gmail.com>
2024-11-12 10:45:22 +0300

    jwt finished

---
+++ controllers/test-jwt.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const router = express.Router();
+
+const user = {
+  _id: 1,
+  username: 'test',
+  password: 'test',
+};
+
+router.get('/sign-token', (req, res) => {
+  const token = jwt.sign({ user }, process.env.JWT_SECRET)
+  res.json({ token });
+});
+
+router.post('/verify-token', (req, res) => {
+  try {
+    const token = req.headers.authorization.split(' ',)[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    res.json({ decoded })
+  } catch (error) {
+    res.status(401).json({ error: 'Invalid Token' });
+  }
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -4,13 +4,18 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 
-// Middleware
+// Controllers
+const testJWTRouter = require('./controllers/test-jwt');
 
+// Middleware
 app.use(express.json());
 
 app.use(morgan("dev"))
 
-// Routes go here
+// Public Routes
+app.use('/test-jwt', testJWTRouter);
+
+// Protected Routes
 
 app.listen(3000, () => {
   console.log('The express app is ready!');
