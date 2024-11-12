7a9cfb33ee905a3f02336d2bf2cdf0f44d73f24e
Mohamed Saleh <mohxbox36021@gmail.com>
2024-11-12 11:30:09 +0300

    sign up and sign in token

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,7 @@
 // /controllers/users.js
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const User = require('../models/user');
 
@@ -16,10 +17,31 @@ router.post('/signup', async (req, res) => {
             username: req.body.username,
             hashedPassword: bcrypt.hashSync(req.body.password, parseInt(process.env.SALT_LENGTH)),
         })
-        res.status(201).json({ user });
+        const token = jwt.sign({ username: user.username, _id: user._id },
+            process.env.JWT_SECRET
+        );
+        res.status(201).json({ user, token });
     } catch (error) {
         console.error(error);
         res.status(400).json({ error: 'Something went wrong!' });
+    }
+});
+
+router.post('/signin', async (req, res) => {
+    try {
+        // Check if the user is found
+        const user = await User.findOne({ username: req.body.username });
+        if (user && bcrypt.compareSync(req.body.password, user.hashedPassword)) {
+            const token = jwt.sign({ username: user.username, _id: user._id },
+                process.env.JWT_SECRET
+            );
+            res.json({ token });
+        } else {
+            throw new Error('Invalid Credentials');
+        }
+    } catch (error) {
+        console.error(error.message);
+        res.status(400).json({ error: 'Invalid Credentials' });
     }
 });
 
